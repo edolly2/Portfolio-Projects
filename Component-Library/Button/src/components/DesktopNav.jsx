8dd4b14dfce4ea6664d7521d56f2272e027af9a5
dollyllama <edolly2@protonmail.com>
2022-12-12 22:00:17 -0600

    first commit

---
+++ Component-Library/Button/src/components/DesktopNav.jsx
@@ -0,0 +1,65 @@
+import styled from 'styled-components';
+import { NavLink } from 'react-router-dom';
+import DarkMode from './DarkMode';
+
+const Container = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: flex-end;
+  justify-content: flex-end;
+  padding: 0 2.4rem;
+`;
+const Nav = styled.nav``;
+const NavList = styled.ul`
+  display: flex;
+  gap: 1.6rem;
+`;
+const NavListItem = styled.li``;
+
+const DesktopNav = (props) => {
+  return (
+    <Container style={props.DesktopNavStyle}>
+      <Nav>
+        <NavList>
+          <DarkMode />
+          <NavListItem>
+            <NavLink
+              style={{ marginLeft: '2.4rem' }}
+              onClick={props.onNavLinkClick}
+              to='/'
+            >
+              Home
+            </NavLink>
+          </NavListItem>
+          <NavListItem>
+            <NavLink onClick={props.onNavLinkClick} to='/about'>
+              About
+            </NavLink>
+          </NavListItem>
+          <NavListItem>
+            <NavLink onClick={props.onNavLinkClick} to='/skills'>
+              Skills
+            </NavLink>
+          </NavListItem>
+          <NavListItem>
+            <NavLink onClick={props.onNavLinkClick} to='/projects'>
+              Projects
+            </NavLink>
+          </NavListItem>
+          <NavListItem>
+            <NavLink onClick={props.onNavLinkClick} to='/contact'>
+              Contact
+            </NavLink>
+          </NavListItem>
+          <NavListItem>
+            <NavLink onClick={props.onNavLinkClick} to='/blog'>
+              Blog
+            </NavLink>
+          </NavListItem>
+        </NavList>
+      </Nav>
+    </Container>
+  );
+};
+
+export default DesktopNav;
---
+++ Component-Library/Button/src/components/MobileMenu.jsx
@@ -0,0 +1,74 @@
+import styled from 'styled-components';
+import { NavLink } from 'react-router-dom';
+import { FaGithub, FaLinkedinIn, FaFacebook, FaTwitter } from 'react-icons/fa';
+
+const MenuContainer = styled.div`
+  position: absolute;
+  top: 100%;
+  right: 0;
+  left: 0;
+  background-color: blue;
+  border-top-left-radius: 0;
+  border-top-right-radius: 0;
+  display: flex;
+  flex-direction: column;
+  gap: 3.2rem;
+  z-index: 4 !important;
+`;
+
+const MobileNav = styled.nav`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+`;
+const MobileNavList = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: 1.6rem;
+  align-items: center;
+`;
+const MobileNavListItem = styled.li``;
+
+const SocialContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 1.6rem;
+`;
+
+const MobileMenu = (props) => {
+  return (
+    <MenuContainer style={props.mobileMenuStyle}>
+      <MobileNav>
+        <MobileNavList>
+          <MobileNavListItem onClick={props.onMenuItemClick}>
+            <NavLink to='/'>Home</NavLink>
+          </MobileNavListItem>
+          <MobileNavListItem onClick={props.onMenuItemClick}>
+            <NavLink to='/about'>About</NavLink>
+          </MobileNavListItem>
+          <MobileNavListItem onClick={props.onMenuItemClick}>
+            <NavLink to='/skills'>Skills</NavLink>
+          </MobileNavListItem>
+          <MobileNavListItem onClick={props.onMenuItemClick}>
+            <NavLink to='/projects'>Projects</NavLink>
+          </MobileNavListItem>
+          <MobileNavListItem onClick={props.onMenuItemClick}>
+            <NavLink to='/contact'>Contact</NavLink>
+          </MobileNavListItem>
+          <MobileNavListItem onClick={props.onMenuItemClick}>
+            <NavLink to='/blog'>Blog</NavLink>
+          </MobileNavListItem>
+        </MobileNavList>
+      </MobileNav>
+      <SocialContainer>
+        <FaGithub className='icon' />
+        <FaLinkedinIn className='icon' />
+        <FaFacebook className='icon' />
+        <FaTwitter className='icon' />
+      </SocialContainer>
+    </MenuContainer>
+  );
+};
+
+export default MobileMenu;
